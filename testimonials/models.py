b81e60a3a58b79eaa2da08a3c9e156379ad3fb99
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-17 05:19:19 +0100

    add model and update urls and views

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Testimonial(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='testimonials/', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.title} by {self.user.username}"
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("Hello from Testimonials App!")
+# This is a temporary view to avoid errors in the testimonials app.
