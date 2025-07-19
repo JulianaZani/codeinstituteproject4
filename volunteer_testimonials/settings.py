715958a3e68e0924306b0b72a93dd3b94183661c
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-19 07:24:02 +0100

    Prepare project for Heroku deployment

--- volunteer_testimonials/settings.py
+++ volunteer_testimonials/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%b3^(78v88egci27e!(*67tkdi)0#*_epzr&(z@uj2)j2*ccsk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testimonials.herokuapp.com', 'localhost']
 
 
 # Application definition
