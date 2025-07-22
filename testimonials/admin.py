b5e16892fdf025b71323a56039819fa1288bbe1a
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-22 07:18:12 +0100

    add admin.py codes

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Testimonial
 
-# Register your models here.
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'approved', 'created_on')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('title', 'content', 'user__username')
