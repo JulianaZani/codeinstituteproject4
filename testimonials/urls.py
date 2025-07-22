d18c5976a1d42988ab5245b3dc5c56994ed73561
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-22 12:27:41 +0100

    add path in testimonials/urls.py

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.testimonial_list, name='testimonial_list'),
     path('testimonial/<int:pk>/', views.testimonial_detail, name='testimonial_detail'),
+    path('testimonial/add/', views.add_testimonial, name='add_testimonial'),
 ]
