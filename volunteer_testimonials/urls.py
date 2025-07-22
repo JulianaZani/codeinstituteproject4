499f5355ba6abadca20b2e50b51c183f12655a8c
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-22 11:51:04 +0100

    delete repetition in volunteer_testimonials/urls.py

--- volunteer_testimonials/urls.py
+++ volunteer_testimonials/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('testimonials.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),  # signup
+    path('accounts/', include('django.contrib.auth.urls')),  # login, logout, password_reset etc.
 ]
