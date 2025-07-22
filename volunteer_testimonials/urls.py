5d273b87343b1b250eaccff861212b0f3de8fbf6
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-22 08:15:36 +0100

    create registration and templates in accounts/ and update urls.py in volunteer_testimonials/

--- volunteer_testimonials/urls.py
+++ volunteer_testimonials/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('testimonials.urls')),
+    path('accounts/signup/', signup_view, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
