416f0827252cabfed15762ec19966bbdf569118c
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-25 21:29:01 +0100

    Add ContactForm to testimonials

--- testimonials/forms.py
+++ testimonials/forms.py
@@ -1,7 +1,6 @@
 from django import forms
-from .models import Testimonial
 
-class TestimonialForm(forms.ModelForm):
-    class Meta:
-        model = Testimonial
-        fields = ['title', 'content', 'image']
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
