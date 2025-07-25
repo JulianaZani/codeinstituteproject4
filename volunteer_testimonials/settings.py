7aea7c043665f5869dfad74d287003fab44befe4
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-25 14:42:15 +0100

    fix cloudinary integration

--- volunteer_testimonials/settings.py
+++ volunteer_testimonials/settings.py
@@ -99,9 +99,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Cloudinary settings
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
