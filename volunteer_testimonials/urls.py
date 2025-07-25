16a9bb7818cbeb55aac5167d80271a8c6f9b4419
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-25 19:12:00 +0100

    update login and signup accounts/templates/

--- volunteer_testimonials/urls.py
+++ volunteer_testimonials/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('testimonials.urls')),
     path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
-# Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
