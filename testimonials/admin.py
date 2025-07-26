9d182130b25de3636021234ec45b8d6862510ba0
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-26 05:17:53 +0100

    fix views.py in testimonials/ accidentally deleted previously

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -6,3 +6,4 @@ class TestimonialAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'approved', 'created_on')
     list_filter = ('approved', 'created_on')
     search_fields = ('title', 'content', 'user__username')
+    
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,5 +1,30 @@
-from .forms import ContactForm
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Testimonial
+from .forms import TestimonialForm, ContactForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
+def testimonial_list(request):
+    testimonials = Testimonial.objects.filter(approved=True)
+    return render(request, 'testimonials/testimonial_list.html', {'testimonials': testimonials})
+
+def testimonial_detail(request, pk):
+    testimonial = get_object_or_404(Testimonial, pk=pk, approved=True)
+    return render(request, 'testimonials/testimonial_detail.html', {'testimonial': testimonial})
+
+@login_required
+def add_testimonial(request):
+    if request.method == 'POST':
+        form = TestimonialForm(request.POST, request.FILES)
+        if form.is_valid():
+            testimonial = form.save(commit=False)
+            testimonial.user = request.user
+            testimonial.save()
+            messages.success(request, 'Your testimonial was submitted and is awaiting approval.')
+            return redirect('testimonial_list')
+    else:
+        form = TestimonialForm()
+    return render(request, 'testimonials/add_testimonial.html', {'form': form})
 
 def contact_view(request):
     if request.method == 'POST':
@@ -10,5 +35,4 @@ def contact_view(request):
             return render(request, 'contact.html', {'form': form, 'success': True})
     else:
         form = ContactForm()
-
     return render(request, 'contact.html', {'form': form})
