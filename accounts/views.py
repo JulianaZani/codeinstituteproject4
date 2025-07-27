a878cc7379637fa3ba15779d0ee339524a939f6c
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-07-27 20:07:57 +0100

    update signup.html

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
+from django.contrib import messages
 
 def signup_view(request):
     if request.method == 'POST':
@@ -9,10 +10,10 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, "Signup successful! Welcome.")
             return redirect('testimonial_list')
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
-
 class CustomLoginView(LoginView):
     template_name = 'accounts/login.html'
