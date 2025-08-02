985aa32fb58c88c519a806811ab7e9192e6cbf81
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-08-02 11:10:00 +0100

    create cloudinary_filters and update img tag in testimonial_detail.html

---
+++ testimonials/templates/testimonials/templatetags/cloudinary_filters.py
@@ -0,0 +1,17 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def cloudinary_optimize(url):
+    """
+    Optimize Cloudinary image URLs by adding transformations
+    for WebP/AVIF, automatic compression, and 800x600 resizing.
+    """
+    if not url or '/upload/' not in url:
+        return url  # Return original if it's not a Cloudinary URL
+
+    return url.replace(
+        '/upload/',
+        '/upload/f_auto,q_auto,w_800,h_600,c_fill/'
+    )
