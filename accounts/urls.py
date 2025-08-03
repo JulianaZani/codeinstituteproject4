02fb60a608ee27771de639f2f3dcb06f2ab463d2
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-08-03 21:48:16 +0100

    fix E501 in urls.py accounts/

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,9 +17,14 @@ urlpatterns = [
         template_name='accounts/password_reset_done.html'
     ), name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='accounts/password_reset_confirm.html'
-    ), name='password_reset_confirm'),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm',
+    ),
+
 
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='accounts/password_reset_complete.html'
