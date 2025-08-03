db345e2860fcb76364d4e8de1d6d592073c36d57
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-08-03 21:30:03 +0100

    fix 5: E302 expected 2 blank lines, found 1

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -10,4 +11,6 @@ class SignUpForm(UserCreationForm):
         fields = ("username", "email", "password1", "password2")
         help_texts = {
             'username': None,
+            'password1': None,
+            'password2': None,
         }
